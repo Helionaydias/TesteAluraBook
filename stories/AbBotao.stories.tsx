59c839418f6bf7570240d8b0f6b9dc8ec787b91b
helionay <helionaydias@hotmail.com>
2024-06-28 17:10:18 -0300

    criando o componente card

--- stories/AbBotao.stories.tsx
+++ stories/AbBotao.stories.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
-import { AbBotao } from '../src';
+import { AbBotao, AbCard } from '../src';
 
 export default {
   title: 'Componentes/AbBotao',
@@ -9,3 +9,11 @@ export default {
 const Template: ComponentStory<typeof AbBotao> = () => <AbBotao />;
 
 export const Primario = Template.bind({});
+
+export default {
+  title: 'Componentes/AbCard',
+} as ComponentMeta<typeof AbCard>;
+
+const Template: ComponentStory<typeof AbBotao> = () => <AbCard />;
+
+export const Secondary = Template.bind({});
